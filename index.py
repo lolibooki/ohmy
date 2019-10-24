930e6e8b61e53b4141c496d9aa7af02047c5d937
lolibooki <araqarib@gmail.com>
2019-10-24 23:29:23 +0330

    Initial commit

---
+++ index.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import requests
+from flask import jsonify, request, make_response, send_from_directory
+
+ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+os.environ.update({'ROOT_PATH': ROOT_PATH})
+sys.path.append(os.path.join(ROOT_PATH, 'modules'))
+
+PUBLIC_PATH = os.path.join(ROOT_PATH, 'public')
+
+from app import app
+
+PORT = os.environ.get('PORT')
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ error handler """
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/')
+def index():
+    """ static files serve """
+    return send_from_directory(PUBLIC_PATH, 'index.html')
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ modules/app/__init__.py
@@ -0,0 +1,31 @@
+import os
+import json
+import datetime
+from bson.objectid import ObjectId
+from flask import Flask
+from flask_pymongo import PyMongo
+from flask_jwt_extended import JWTManager
+
+
+class JSONEncoder(json.JSONEncoder):
+    ''' extend json-encoder class '''
+
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, set):
+            return list(o)
+        if isinstance(o, datetime.datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = "mongodb://localhost:27017/"
+app.config['JWT_SECRET_KEY'] = os.environ.get('SECRET')
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
+mongo = PyMongo(app)
+jwt = JWTManager(app)
+app.json_encoder = JSONEncoder
+
+from app.controllers import *  # pylint: disable=W0401,C0413
